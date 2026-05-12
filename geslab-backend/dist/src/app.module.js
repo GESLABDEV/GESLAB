58bd6bb117b4e379f9b68e5aaded659c08be4bc6
GESLABDEV <geslab.ir@gmail.com>
2026-05-11 20:01:05 -0500

    build :building_construction: (core) :necktie: Integración a backend: NestJS:3001 + Swagger, Next.js :3000/login + shadcn

---
+++ geslab-backend/dist/src/app.module.js
@@ -0,0 +1,33 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const auth_module_1 = require("./auth/auth.module");
+const users_module_1 = require("./users/users.module");
+const novelties_module_1 = require("./novelties/novelties.module");
+const requests_module_1 = require("./requests/requests.module");
+const shifts_module_1 = require("./shifts/shifts.module");
+const compliance_module_1 = require("./compliance/compliance.module");
+const surveys_module_1 = require("./surveys/surveys.module");
+const notifications_module_1 = require("./notifications/notifications.module");
+const reports_module_1 = require("./reports/reports.module");
+const engine_module_1 = require("./engine/engine.module");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [auth_module_1.AuthModule, users_module_1.UsersModule, novelties_module_1.NoveltiesModule, requests_module_1.RequestsModule, shifts_module_1.ShiftsModule, compliance_module_1.ComplianceModule, surveys_module_1.SurveysModule, notifications_module_1.NotificationsModule, reports_module_1.ReportsModule, engine_module_1.EngineModule],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
